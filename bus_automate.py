d09754e50d543c5fdcce6f7d943b3757602ce46b
pratik7368patil <38435193+pratik7368patil@users.noreply.github.com>
2019-10-22 20:01:08 +0530

    Minor stuff automated.

--- bus_automate.py
+++ bus_automate.py
@@ -4,6 +4,10 @@ import time
 url = "https://www.redbus.in/"
 driver = webdriver.Chrome()
 driver.get(url)
-des_city = driver.find_element_by_id('src').send_keys('Malkapur')
+s_city = driver.find_element_by_id('src').send_keys('Malkapur')
 time.sleep(5)
+d_city = driver.find_element_by_id('dest').send_keys('Pune')
+time.sleep(5)
+dr = driver.find_element_by_id('onward_cal')
+dr.send_keys('23112019')
 
